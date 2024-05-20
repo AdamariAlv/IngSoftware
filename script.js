83d4e554bc2571c419cf27bd62dd239775b66f6c
Mercedes Mendoza <142846043+AdamariAlv@users.noreply.github.com>
2024-05-20 13:49:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -52,3 +52,31 @@ document.addEventListener("DOMContentLoaded", function() {
 const totalResultadosLabel = document.getElementById('total-resultados');
 totalResultadosLabel.textContent = `Total: ${resultsCount} resultado(s)`;
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Obtener el elemento del calendario de fecha inicial
+    var startDateInput = document.getElementById("start-date");
+
+    // Agregar un listener para el evento de cambio en la fecha
+    startDateInput.addEventListener("change", function() {
+        // Obtener la fecha actual
+        var today = new Date();
+        
+        // Obtener la fecha seleccionada en el calendario
+        var selectedDate = new Date(this.value);
+        
+        // Calcular la diferencia en milisegundos
+        var differenceInMilliseconds = selectedDate.getTime() - today.getTime();
+        
+        // Convertir la diferencia a días
+        var differenceInDays = differenceInMilliseconds / (1000 * 60 * 60 * 24);
+        
+        // Verificar si la diferencia es menor o igual a 4 días
+        if (differenceInDays <= 4) {
+            alert("La fecha de inicio del viaje debe ser con anticipacion mínima de 4 días a partir de la fecha actual.");
+            this.value = "";
+        }
+    });
+});
+
